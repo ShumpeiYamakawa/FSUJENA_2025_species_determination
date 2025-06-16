37f07051cf943ed00f75451281cab917f94b8874
ShumpeiYamakawa <shumpei.yamakawa@gmail.com>
2025-06-16 14:57:40 +0200

    Delete sratools_install.sh

--- sratools_install.sh
+++
@@ -1,5 +0,0 @@
-cd ~/test_meta
-wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz
-tar -zxvf sratoolkit.current-ubuntu64.tar.gz
-cd sratoolkit.3.2.1-ubuntu64/bin
-echo "export PATH=\"$(pwd):\$PATH\"" >> ~/.bashrc; source ~/.bashrc
